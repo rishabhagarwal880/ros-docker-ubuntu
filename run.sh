b27836bc135ce1d97f2609fa2e39e43f9b0d01b9
Rishabh <rishabhagarwal880@gmail.com>
2017-09-14 13:44:57 -0400

    xhost working perfectly

--- run.sh
+++ run.sh
@@ -17,15 +17,9 @@ set -e
 docker run -it\
   --net=host\
   -e SHELL\
-  -e DISPLAY\
+  -e DISPLAY=$DISPLAY\
   -e DOCKER=1\
   -v "$HOME:$HOME:rw"\
-  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
-  -it $1 $SHELL\
-  --user=$USER \
-  --workdir="/home/$USER" \
-  --volume="/home/$USER:/home/$USER" \
-  --volume="/etc/group:/etc/group:ro" \
-  --volume="/etc/passwd:/etc/passwd:ro" \
-  --volume="/etc/shadow:/etc/shadow:ro" \
-  --volume="/etc/sudoers.d:/etc/sudoers.d:ro" 
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -e "QT_X11_NO_MITSHM=1"\
+  -it $1 $SHELL  
